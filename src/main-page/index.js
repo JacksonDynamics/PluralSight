ca7040675418a2db5a039bb7c5bc16c987474ff0
Brendon Jackson <64535078+JacksonDynamics@users.noreply.github.com>
2022-04-28 20:21:15 -0600

    Globomantics

---
+++ src/main-page/index.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './main-page.css';
+import Header from "./header"
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+     
+  );
+}
+
+export default App;
